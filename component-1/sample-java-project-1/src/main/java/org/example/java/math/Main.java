f3380e016da6032172bc33e76064f7617dadc802
binodmx <binod@wso2.com>
2022-10-21 16:51:51 +0530

    Modify comment

--- component-1/sample-java-project-1/src/main/java/org/example/java/math/Main.java
+++ component-1/sample-java-project-1/src/main/java/org/example/java/math/Main.java
@@ -2,7 +2,7 @@ package org.example.java.math;
 
 public class Main {
     public static void main(String[] args) {
-        // print hello world
+        // print hello world msg
         System.out.println("Hello, World!");
     }
 }

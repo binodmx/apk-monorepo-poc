2efda2c6e0b9c9a1226a92118d67e02ecb0c677a
Binod Karunanayake <36144069+binodmx@users.noreply.github.com>
2022-11-08 13:23:12 +0530

    Update MyClass.java

--- codecov-sp-test/src/main/java/org/example/java/codecov/MyClass.java
+++ codecov-sp-test/src/main/java/org/example/java/codecov/MyClass.java
@@ -10,4 +10,12 @@ public class MyClass {
     public String getHello2() {
         return "Hello2";
     }
+    
+    public String getHello3() {
+        return "Hello3";
+    }
+    
+    public String getHello4() {
+        return "Hello4";
+    }
 }

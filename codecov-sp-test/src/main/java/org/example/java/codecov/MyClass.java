be884b87abace70ba4756f52b9bd539421149dc6
binodmx <binod@wso2.com>
2022-11-08 12:52:53 +0530

    Revert "Update MyClass.java"
    This reverts commit 6884fe89829752860432440bc0c48371e249eed6.

--- codecov-sp-test/src/main/java/org/example/java/codecov/MyClass.java
+++ codecov-sp-test/src/main/java/org/example/java/codecov/MyClass.java
@@ -2,12 +2,16 @@ package org.example.java.codecov;
 
 
 public class MyClass {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 
     public String getHello() {
         return "Hello";
     }
     
     public String getHello2() {
-        return "Hello2";
+        return "Hello";
     }
 }

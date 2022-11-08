0ea7c76e0fa8768717baadd4f30bf0c66e47db2f
Binod Karunanayake <36144069+binodmx@users.noreply.github.com>
2022-11-08 12:53:39 +0530

    Update MyClass.java

--- codecov-sp-test/src/main/java/org/example/java/codecov/MyClass.java
+++ codecov-sp-test/src/main/java/org/example/java/codecov/MyClass.java
@@ -2,10 +2,6 @@ package org.example.java.codecov;
 
 
 public class MyClass {
-    
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
 
     public String getHello() {
         return "Hello";

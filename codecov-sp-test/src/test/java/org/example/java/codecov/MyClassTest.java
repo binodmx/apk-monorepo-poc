abc936c37a0f24b37019e45ad289bd0b98b1c6ce
Binod Karunanayake <36144069+binodmx@users.noreply.github.com>
2022-11-08 14:57:51 +0530

    Update MyClassTest.java

--- codecov-sp-test/src/test/java/org/example/java/codecov/MyClassTest.java
+++ codecov-sp-test/src/test/java/org/example/java/codecov/MyClassTest.java
@@ -12,27 +12,27 @@ public class MyClassTest {
         assertEquals(myClass.getHello(), "Hello");
     }
     
-    @Test
-    public void testGetHello2() {
-        MyClass myClass = new MyClass();
-        assertEquals(myClass.getHello2(), "Hello2");
-    }
+//     @Test
+//     public void testGetHello2() {
+//         MyClass myClass = new MyClass();
+//         assertEquals(myClass.getHello2(), "Hello2");
+//     }
     
-    @Test
-    public void testGetHello3() {
-        MyClass myClass = new MyClass();
-        assertEquals(myClass.getHello3(), "Hello3");
-    }
+//     @Test
+//     public void testGetHello3() {
+//         MyClass myClass = new MyClass();
+//         assertEquals(myClass.getHello3(), "Hello3");
+//     }
     
-    @Test
-    public void testGetHello4() {
-        MyClass myClass = new MyClass();
-        assertEquals(myClass.getHello4(), "Hello4");
-    }
+//     @Test
+//     public void testGetHello4() {
+//         MyClass myClass = new MyClass();
+//         assertEquals(myClass.getHello4(), "Hello4");
+//     }
     
-    @Test
-    public void testGetHello5() {
-        MyClass myClass = new MyClass();
-        assertEquals(myClass.getHello5(), "Hello5");
-    }
+//     @Test
+//     public void testGetHello5() {
+//         MyClass myClass = new MyClass();
+//         assertEquals(myClass.getHello5(), "Hello5");
+//     }
 }
